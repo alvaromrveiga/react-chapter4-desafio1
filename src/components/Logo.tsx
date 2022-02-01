a0e2bc98ec4f5af21728ec9758ca9d989728763d
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-02-01 13:03:44 -0300

    Add logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,14 @@
+import { Flex, Image, Link as ChakraLink } from "@chakra-ui/react";
+import Link from "next/link";
+
+export function Logo() {
+  return (
+    <Flex w="100vw" align="center" justify="center">
+      <Link passHref href="/">
+        <ChakraLink>
+          <Image paddingTop="6" paddingBottom="6" src="Logo.svg" alt="logo" />
+        </ChakraLink>
+      </Link>
+    </Flex>
+  );
+}
