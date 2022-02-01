4a66ac46170f3778185ab25b101e5219d5b9c035
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-02-01 13:03:29 -0300

    Add home banner

---
+++ src/components/Home/HomeBanner.tsx
@@ -0,0 +1,30 @@
+import { HStack, Image, Stack, Text } from "@chakra-ui/react";
+
+export function HomeBanner() {
+  return (
+    <HStack
+      justifyContent="space-between"
+      backgroundImage={"sky.jpg"}
+      backgroundSize="cover"
+      backgroundRepeat="no-repeat"
+    >
+      <Stack paddingLeft="140px">
+        <Text fontWeight="500" fontSize={36} color="gray.50">
+          5 Continentes, infinitas possibilidades.
+        </Text>
+        <Text fontWeight="400" fontSize={20} color="gray.300">
+          Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+        </Text>
+      </Stack>
+
+      <Image
+        position="relative"
+        top="33px"
+        paddingRight="140px"
+        src="Airplane.svg"
+        alt="airplane"
+        transform="rotate(3deg)"
+      />
+    </HStack>
+  );
+}
