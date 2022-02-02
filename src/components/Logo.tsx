0e9bd9187059c8b0a345bdd2d505cda7e08c0c8b
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-02-02 18:31:08 -0300

    Get logo from icons folder

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,12 @@ export function Logo() {
     <Flex w="100vw" align="center" justify="center">
       <Link passHref href="/">
         <ChakraLink>
-          <Image paddingTop="6" paddingBottom="6" src="Logo.svg" alt="logo" />
+          <Image
+            paddingTop="6"
+            paddingBottom="6"
+            src="icons/Logo.svg"
+            alt="logo"
+          />
         </ChakraLink>
       </Link>
     </Flex>
