cdbc777865e7bf26e402a28f171a045451ed6553
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-02-02 18:31:50 -0300

    Fix home swiper path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { HomeBanner } from "../components/Home/HomeBanner";
 import { HomeIcons } from "../components/Home/HomeIcons";
-import { HomeSwiper } from "../components/Home/HomeSwiper";
+import { HomeSwiper } from "../components/Home/Swiper/HomeSwiper";
 import { Logo } from "../components/Logo";
 
 const Home: NextPage = () => {
