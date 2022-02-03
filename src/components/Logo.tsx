66b2af52575110380a2c7170f2991c23da0b001f
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-02-03 14:11:11 -0300

    Add optional return arrow

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,19 +1,39 @@
-import { Flex, Image, Link as ChakraLink } from "@chakra-ui/react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Box, Flex, Image, Link as ChakraLink } from "@chakra-ui/react";
 import Link from "next/link";
 
-export function Logo() {
+interface LogoProps {
+  returnArrow?: boolean;
+}
+
+export function Logo({ returnArrow = false }: LogoProps) {
   return (
-    <Flex w="100vw" align="center" justify="center">
+    <Box w="100vw">
       <Link passHref href="/">
-        <ChakraLink>
-          <Image
-            paddingTop="6"
-            paddingBottom="6"
-            src="icons/Logo.svg"
-            alt="logo"
-          />
-        </ChakraLink>
+        <Flex align="center" justify="space-between" marginX="8.75rem">
+          {returnArrow ? (
+            <ChakraLink>
+              <ArrowBackIcon w="2rem" h="2rem" />
+            </ChakraLink>
+          ) : (
+            <Box w="2rem" h="2rem" />
+          )}
+
+          <ChakraLink>
+            <Image
+              mr="2rem"
+              w="11.5rem"
+              h="2.875rem"
+              mt="1.5rem"
+              mb="1.5rem"
+              src="icons/Logo.svg"
+              alt="logo"
+            />
+          </ChakraLink>
+
+          <Box />
+        </Flex>
       </Link>
-    </Flex>
+    </Box>
   );
 }
